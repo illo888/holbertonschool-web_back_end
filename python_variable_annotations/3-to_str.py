223a5bfcf27cbe7ad45c063c96eaade5a624c85d
illo888 <shadeenn1424@gmail.com>
2026-02-17 10:18:41 +0000

    Fix E501 and update to_str implementation

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Module that provides a function to convert a float to its string representation.
+Module that provides a function to convert a float
+to its string representation.
 """
 
 
