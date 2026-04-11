418143383dba93d2390fa8c758e256028c78a496
Shaden <shadeenn1424@gmail.com>
2026-04-11 09:47:35 +0300

    Fix name output in stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,7 @@
-#!/usr/bin/node
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
